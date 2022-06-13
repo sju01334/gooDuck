ce13e6ff8ff34a3bc8092d8f9b57e053f902b557
sju01334 <sju01334@gmail.com>
2022-06-13 09:36:56 +0900

    #10 카드 관리 페이지 xml 작성

---
+++ app/src/main/java/com/nepplus/gooduck/ui/setting/CardAddActivity.kt
@@ -0,0 +1,23 @@
+package com.nepplus.gooduck.ui.setting
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nepplus.gooduck.BaseActivity
+import com.nepplus.gooduck.R
+
+class CardAddActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_card_add)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+}
---
+++ app/src/main/java/com/nepplus/gooduck/ui/setting/CardManagementActivity.kt
@@ -0,0 +1,22 @@
+package com.nepplus.gooduck.ui.setting
+
+import android.os.Bundle
+import com.nepplus.gooduck.BaseActivity
+import com.nepplus.gooduck.R
+
+class PayManagementActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_card_management)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+}
