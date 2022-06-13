9c4c7c9606275cda0804a4996c97a37fb924c80a
sju01334 <sju01334@gmail.com>
2022-06-13 09:37:38 +0900

    #11 내 리뷰 목록 xml 작성

---
+++ app/src/main/java/com/nepplus/gooduck/ui/setting/MyReviewListActivity.kt
@@ -0,0 +1,23 @@
+package com.nepplus.gooduck.ui.setting
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nepplus.gooduck.BaseActivity
+import com.nepplus.gooduck.R
+
+class MyReviewListActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_review_list)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+}
