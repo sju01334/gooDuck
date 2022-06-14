14b889798ae5c19bbf794cf6b2778310bce46b1b
sju01334 <sju01334@gmail.com>
2022-06-14 11:13:59 +0900

    #20 사이드 toolbar로 변경하여 구현  => 여전히 클릭이벤트는 작동하지 않음

--- app/src/main/java/com/nepplus/gooduck/ui/main/MainActivity.kt
+++ app/src/main/java/com/nepplus/gooduck/ui/main/MainActivity.kt
@@ -3,20 +3,25 @@ package com.nepplus.gooduck.ui.main
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.MenuItem
 import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.view.GravityCompat
 import androidx.databinding.DataBindingUtil
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.navigation.NavigationView
 import com.nepplus.gooduck.BaseActivity
 import com.nepplus.gooduck.R
+import com.nepplus.gooduck.R.*
 import com.nepplus.gooduck.adapters.MainViewPagerAdapter
+import com.nepplus.gooduck.databinding.ActivityCardAddBinding.inflate
 import com.nepplus.gooduck.databinding.ActivityMainBinding
 import com.nepplus.gooduck.ui.market.CartActivity
 
 
-class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedListener {
+class MainActivity : BaseActivity(){
 
     lateinit var binding : ActivityMainBinding
 
@@ -24,7 +29,7 @@ class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedList
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, layout.activity_main)
         setupEvents()
         setValues()
     }
@@ -32,24 +37,35 @@ class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedList
     override fun setupEvents() {
 
 
+        setSupportActionBar(binding.mainToolbar)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)  // 왼쪽 버튼 사용 여부 true
+        supportActionBar!!.setHomeAsUpIndicator(drawable.three_line)
+        supportActionBar!!.setDisplayShowTitleEnabled(false)
 
-//        val toggle = ActionBarDrawerToggle(this, binding.drawerLayout, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
-//        binding.drawerLayout.addDrawerListener(toggle)
-//        toggle.syncState()
-//
-//        binding.mainToolbar.bagBtn.setOnClickListener {
-//            val myInent = Intent(mContext, CartActivity::class.java)
-//            startActivity(myInent)
-//        }
-//
-//        binding.mainToolbar.backBtn.setOnClickListener {
-//            finish()
-//        }
+        val toggle = ActionBarDrawerToggle(this, binding.drawerLayout,binding.mainToolbar, string.navigation_drawer_open, string.navigation_drawer_close)
+        binding.drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
 
-        sideBarBtn.setOnClickListener {
-            binding.drawerLayout.openDrawer(binding.navView)
-        }
-        binding.navView.setNavigationItemSelectedListener(this)
+        binding.navView.setNavigationItemSelectedListener(object : NavigationView.OnNavigationItemSelectedListener{
+            override fun onNavigationItemSelected(item: MenuItem): Boolean {
+                when (item.itemId) {
+                    id.nav_home -> {
+
+                        Toast.makeText(mContext, "1111", Toast.LENGTH_SHORT).show()
+                        Log.d("눌렸니", "yes")
+                        val myIntent = Intent(mContext, CartActivity::class.java)
+                        startActivity(myIntent)
+
+                    }
+                    id.nav_gallery -> {}
+                    else  -> {}
+                }
+                binding.drawerLayout.closeDrawer(GravityCompat.START)
+                return true
+            }
+        })
+
+
 
 
     }
@@ -62,7 +78,7 @@ class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedList
 
         binding.mainViewPager.currentItem = 1
         binding.mainViewPager.isUserInputEnabled = false
-        binding.bottomNav.selectedItemId = R.id.market
+        binding.bottomNav.selectedItemId = id.market
 
         binding.mainViewPager.registerOnPageChangeCallback(
             object : ViewPager2.OnPageChangeCallback() {
@@ -74,28 +90,39 @@ class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedList
 
         binding.bottomNav.setOnItemSelectedListener {
             when (it.itemId) {
-                R.id.review -> binding.mainViewPager.currentItem = 0
-                R.id.market -> binding.mainViewPager.currentItem = 1
-                R.id.settings -> binding.mainViewPager.currentItem = 2
+                id.review -> binding.mainViewPager.currentItem = 0
+                id.market -> binding.mainViewPager.currentItem = 1
+                id.settings -> binding.mainViewPager.currentItem = 2
             }
             return@setOnItemSelectedListener true
         }
     }
 
-    override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            R.id.nav_home -> {
-                Toast.makeText(mContext, "1111", Toast.LENGTH_SHORT).show()
-                Log.d("눌렸니", "yes")
-                val myIntent = Intent(mContext, CartActivity::class.java)
-                startActivity(myIntent)
-
-            }
-            R.id.nav_gallery -> {}
-            else  -> {}
-        }
-        binding.drawerLayout.closeDrawer(GravityCompat.START)
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.top_nav_menu, menu)
         return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            id.topNavCart -> {
+                val myInent = Intent(mContext, CartActivity::class.java)
+                startActivity(myInent)
+            }
+            else ->{
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onBackPressed() {
+        if(binding.drawerLayout.isDrawerOpen(GravityCompat.START)){
+            binding.drawerLayout.closeDrawer(GravityCompat.START)
+        }else{
+            super.onBackPressed()
+        }
+
     }
 
 }
