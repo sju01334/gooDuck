5db0ab7a11dec4f9884da967fe20e7b8b07b985a
sju01334 <sju01334@gmail.com>
2022-06-18 19:02:29 +0900

    #23 material ui 의  TextInputLayout이용 passwordToggleEnabled = "true" 설정

--- app/src/main/java/com/nepplus/gooduck/BaseActivity.kt
+++ app/src/main/java/com/nepplus/gooduck/BaseActivity.kt
@@ -3,6 +3,10 @@ package com.nepplus.gooduck
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import android.widget.FrameLayout
 import android.widget.ImageView
 import android.widget.TextView
@@ -13,6 +17,7 @@ import com.nepplus.gooduck.api.APIList
 import com.nepplus.gooduck.api.ServerApi
 import com.nepplus.gooduck.ui.market.CartActivity
 import retrofit2.Retrofit
+
 
 abstract class BaseActivity : AppCompatActivity() {
 
@@ -69,5 +74,22 @@ abstract class BaseActivity : AppCompatActivity() {
             finish()
         }
 
+    }
+
+    override fun dispatchTouchEvent(ev: MotionEvent): Boolean {
+        val view: View? = currentFocus
+        if (view != null && (ev.action == MotionEvent.ACTION_UP || ev.action == MotionEvent.ACTION_MOVE) && view is EditText
+        ) {
+            val scrcoords = IntArray(2)
+            view.getLocationOnScreen(scrcoords)
+            val x: Float = ev.rawX + view.getLeft() - scrcoords[0]
+            val y: Float = ev.rawY + view.getTop() - scrcoords[1]
+            if (x < view.getLeft() || x > view.getRight() || y < view.getTop() || y > view.getBottom()) (this.getSystemService(
+                INPUT_METHOD_SERVICE
+            ) as InputMethodManager).hideSoftInputFromWindow(
+                this.window.decorView.applicationWindowToken, 0
+            )
+        }
+        return super.dispatchTouchEvent(ev)
     }
 }
