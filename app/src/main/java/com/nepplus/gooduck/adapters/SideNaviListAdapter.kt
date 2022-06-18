9775ebd116f31471a7440fbfff046d279399cc3b
sju01334 <sju01334@gmail.com>
2022-06-19 00:59:29 +0900

    #20 사이드바 카테고리 목록 추가

---
+++ app/src/main/java/com/nepplus/gooduck/adapters/SideNaviListAdapter.kt
@@ -0,0 +1,86 @@
+package com.nepplus.gooduck.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.*
+import com.nepplus.gooduck.R
+import com.nepplus.gooduck.models.BasicResponse
+import com.nepplus.gooduck.models.Category
+import com.nepplus.gooduck.models.SmallCategory
+import retrofit2.Callback
+import kotlin.collections.ArrayList
+
+class SideNaviListAdapter(
+    val mContext: Context,
+    val titleList: List<String>,
+    val dataList: HashMap<String, List<String>>
+) : BaseExpandableListAdapter(){
+
+    override fun getGroupCount(): Int {
+        return titleList.size
+    }
+
+    override fun getChildrenCount(position: Int): Int {
+        return dataList[titleList[position]]?.size ?: 0
+    }
+
+    override fun getGroup(position: Int): Any {
+        return titleList[position]
+    }
+
+    override fun getChild(parentPosition: Int, childPosition: Int): Any {
+        return dataList[titleList[parentPosition]]!![childPosition]
+    }
+
+    override fun getGroupId(position: Int): Long {
+        return position.toLong()
+    }
+
+    override fun getChildId(parentPosition: Int, childPosition: Int): Long {
+        return childPosition.toLong()
+    }
+
+    override fun hasStableIds(): Boolean {
+        return false
+    }
+
+    override fun getGroupView(position: Int, isExpanded: Boolean, convertView: View?, parent: ViewGroup?): View {
+        var tempRow = convertView
+        if(tempRow == null){
+            tempRow = LayoutInflater.from(mContext).inflate(R.layout.item_side_navi_parent, null)
+        }
+        var row = tempRow!!
+
+        val bigTxt = tempRow.findViewById<TextView>(R.id.bigTxt)
+        val plusminusBtn = tempRow.findViewById<ImageView>(R.id.plusminusBtn)
+
+        if (isExpanded)
+            plusminusBtn.setImageResource(R.drawable.plus);
+        else
+            plusminusBtn.setImageResource(R.drawable.minus);
+
+        bigTxt.text = titleList[position]
+
+        return row
+    }
+
+    override fun getChildView(parentPosition: Int, childPosition: Int, isExpanded: Boolean, convertView: View?, parent: ViewGroup?): View {
+        var tempRow = convertView
+        if(tempRow == null){
+            tempRow = LayoutInflater.from(mContext).inflate(R.layout.item_side_navi_child, null)
+        }
+        var row = tempRow!!
+
+        val smallTxt = tempRow.findViewById<TextView>(R.id.smallTxt)
+
+        smallTxt.text = getChild(parentPosition, childPosition).toString()
+
+        return row
+    }
+
+    override fun isChildSelectable(p0: Int, p1: Int): Boolean {
+        return true
+    }
+}
--- app/src/main/java/com/nepplus/gooduck/ui/main/MainActivity.kt
+++ app/src/main/java/com/nepplus/gooduck/ui/main/MainActivity.kt
@@ -2,29 +2,32 @@ package com.nepplus.gooduck.ui.main
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
-import android.widget.*
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.content.ContextCompat
 import androidx.core.view.GravityCompat
 import androidx.databinding.DataBindingUtil
 import androidx.viewpager2.widget.ViewPager2
-import com.google.android.material.navigation.NavigationView
 import com.nepplus.gooduck.BaseActivity
 import com.nepplus.gooduck.R
 import com.nepplus.gooduck.R.*
 import com.nepplus.gooduck.adapters.MainViewPagerAdapter
+import com.nepplus.gooduck.adapters.SideNaviListAdapter
 import com.nepplus.gooduck.databinding.ActivityMainBinding
 import com.nepplus.gooduck.dialog.CustomAlertDialog
+import com.nepplus.gooduck.models.BasicResponse
+import com.nepplus.gooduck.models.Category
 import com.nepplus.gooduck.ui.market.CartActivity
 import com.nepplus.gooduck.ui.setting.MyReviewListActivity
 import com.nepplus.gooduck.ui.setting.PaymentListActivity
 import com.nepplus.gooduck.ui.setting.PointListActivity
 import com.nepplus.gooduck.utils.ContextUtil
 import com.nepplus.gooduck.utils.GlobalData
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 
 class MainActivity : BaseActivity(){
@@ -33,37 +36,26 @@ class MainActivity : BaseActivity(){
 
     lateinit var mPagerAdapter : MainViewPagerAdapter
 
+    var mCategoryList = ArrayList<Category>()
+
+    var titleList = ArrayList<String>()
+    val dataList = HashMap<String, List<String>>()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, layout.activity_main)
+        getCategoryList()
         setupEvents()
         setValues()
+
     }
 
     override fun setupEvents() {
-
-
-        setSupportActionBar(binding.mainToolbar)
-        supportActionBar!!.setDisplayHomeAsUpEnabled(true)  // 왼쪽 버튼 사용 여부 true
-        supportActionBar!!.setHomeAsUpIndicator(drawable.three_line)
-        supportActionBar!!.setDisplayShowTitleEnabled(false)
-
-        val toggle = ActionBarDrawerToggle(this, binding.drawerLayout,binding.mainToolbar, string.navigation_drawer_open, string.navigation_drawer_close)
-        binding.drawerLayout.addDrawerListener(toggle)
-        toggle.syncState()
-
 //        네비 헤더 메뉴 클릭 리스너
-        val header = binding.navView.getHeaderView(0)
-        val logout = header.findViewById<Button>(R.id.logoutBtn)
-        val myPage = header.findViewById<Button>(R.id.myPageBtn)
-        val txt = header.findViewById<TextView>(id.Txt)
-        val manageBuyLayout = header.findViewById<LinearLayout>(id.manageBuyLayout)
-        val manageReviewLayout = header.findViewById<LinearLayout>(id.manageReviewLayout)
-        val managePointLayout = header.findViewById<LinearLayout>(id.managePointLayout)
+        binding.sideNaviHeader.Txt.text = "안녕하세요. ${GlobalData.loginUser!!.nickname}님, 환영합니다!"
 
-        txt.text = "안녕하세요. ${GlobalData.loginUser!!.nickname}님, 환영합니다!"
-
-        logout.setOnClickListener {
+        binding.sideNaviHeader.logoutBtn.setOnClickListener {
             //        로그아웃
             val alert = CustomAlertDialog(mContext)
             alert.myDialog(object : CustomAlertDialog.ButtonClickListener{
@@ -87,59 +79,53 @@ class MainActivity : BaseActivity(){
             alert.binding.positiveBtn.setBackgroundResource(R.drawable.r5_red_stroke_1dp)
         }
 
-        myPage.setOnClickListener {
+        binding.sideNaviHeader.myPageBtn.setOnClickListener {
             binding.drawerLayout.closeDrawer(GravityCompat.START)
             binding.mainViewPager.currentItem = 2
         }
 
-        manageReviewLayout.setOnClickListener{
+        binding.sideNaviHeader.manageReviewLayout.setOnClickListener{
             binding.drawerLayout.closeDrawer(GravityCompat.START)
             val myIntent = Intent(mContext, MyReviewListActivity::class.java)
             startActivity(myIntent)
         }
 
-        manageBuyLayout.setOnClickListener {
+        binding.sideNaviHeader.manageBuyLayout.setOnClickListener {
             binding.drawerLayout.closeDrawer(GravityCompat.START)
             val myIntent = Intent(mContext, PaymentListActivity::class.java)
             startActivity(myIntent)
         }
 
-        managePointLayout.setOnClickListener {
+        binding.sideNaviHeader.managePointLayout.setOnClickListener {
             binding.drawerLayout.closeDrawer(GravityCompat.START)
             val myIntent = Intent(mContext, PointListActivity::class.java)
             startActivity(myIntent)
         }
 
 
-
-
-        binding.navView.setNavigationItemSelectedListener(object : NavigationView.OnNavigationItemSelectedListener{
-            override fun onNavigationItemSelected(item: MenuItem): Boolean {
-                when (item.itemId) {
-                    id.nav_home -> {
-
-//                        Toast.makeText(mContext, "1111", Toast.LENGTH_SHORT).show()
-//                        Log.d("눌렸니", "yes")
-//                        val myIntent = Intent(mContext, CartActivity::class.java)
-//                        startActivity(myIntent)
-
-                    }
-                    id.nav_gallery -> {}
-                    else  -> {}
-                }
-                binding.drawerLayout.closeDrawer(GravityCompat.START)
-                return false
-            }
-        })
-
-
-
+//        binding.navView.setNavigationItemSelectedListener(object : NavigationView.OnNavigationItemSelectedListener{
+//            override fun onNavigationItemSelected(item: MenuItem): Boolean {
+//                when (item.itemId) {
+//                    id.nav_home -> {
+//
+////                        Toast.makeText(mContext, "1111", Toast.LENGTH_SHORT).show()
+////                        Log.d("눌렸니", "yes")
+////                        val myIntent = Intent(mContext, CartActivity::class.java)
+////                        startActivity(myIntent)
+//
+//                    }
+//                    id.nav_gallery -> {}
+//                    else  -> {}
+//                }
+//                binding.drawerLayout.closeDrawer(GravityCompat.START)
+//                return false
+//            }
+//        })
 
 
     }
 
     override fun setValues() {
-
 
         mPagerAdapter = MainViewPagerAdapter(this)
         binding.mainViewPager.adapter = mPagerAdapter
@@ -147,6 +133,8 @@ class MainActivity : BaseActivity(){
         binding.mainViewPager.currentItem = 1
         binding.mainViewPager.isUserInputEnabled = false
         binding.bottomNav.selectedItemId = id.market
+
+
 
         binding.mainViewPager.registerOnPageChangeCallback(
             object : ViewPager2.OnPageChangeCallback() {
@@ -164,6 +152,17 @@ class MainActivity : BaseActivity(){
             }
             return@setOnItemSelectedListener true
         }
+
+
+        setSupportActionBar(binding.mainToolbar)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)  // 왼쪽 버튼 사용 여부 true
+        supportActionBar!!.setHomeAsUpIndicator(drawable.three_line)
+        supportActionBar!!.setDisplayShowTitleEnabled(false)
+
+        val toggle = ActionBarDrawerToggle(this, binding.drawerLayout,binding.mainToolbar, string.navigation_drawer_open, string.navigation_drawer_close)
+        binding.drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -191,6 +190,42 @@ class MainActivity : BaseActivity(){
             super.onBackPressed()
         }
 
+    }
+
+    fun getCategoryList(){
+        apiList.getRequestAllCategory().enqueue(object : Callback<BasicResponse> {
+            override fun onResponse(call: Call<BasicResponse>, response: Response<BasicResponse>) {
+                if(response.isSuccessful){
+                    val br = response.body()!!
+                    mCategoryList.addAll(br.data.categories)
+                }
+            }
+            override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
+            }
+        })
+
+        apiList.getRequestAllCategory().enqueue(object : Callback<BasicResponse> {
+            override fun onResponse(call: Call<BasicResponse>, response: Response<BasicResponse>) {
+                if(response.isSuccessful){
+                    val br = response.body()!!
+                    mCategoryList.addAll(br.data.categories)
+                    val name = br.data.categories.map { it.name }
+                    titleList.addAll(name)
+                    for( i in 0 until titleList.size){
+                        val child = ArrayList<String>()
+                        child.addAll(br.data.categories[i].smallCategories.map { it.name })
+                        dataList[name[i]] = child
+                    }
+
+                    titleList = ArrayList(dataList.keys)
+                    val expandAdapter = SideNaviListAdapter(mContext, titleList, dataList)
+                    binding.expandListView.setAdapter(expandAdapter)
+
+                }
+            }
+            override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
+            }
+        })
     }
 
 }
